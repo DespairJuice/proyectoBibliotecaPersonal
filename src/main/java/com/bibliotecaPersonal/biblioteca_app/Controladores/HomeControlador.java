91b170659cfe356f659c4864ce6d83835cd527eb
Desesspere <juanjonir@gmail.com>
2025-11-12 19:40:04 -0500

    Creación de controlador y endpoint para HomeScreen

---
+++ src/main/java/com/bibliotecaPersonal/biblioteca_app/Controladores/HomeControlador.java
@@ -0,0 +1,13 @@
+package com.bibliotecaPersonal.biblioteca_app.Controladores;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeControlador {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Biblioteca Personal API";
+    }
+}
