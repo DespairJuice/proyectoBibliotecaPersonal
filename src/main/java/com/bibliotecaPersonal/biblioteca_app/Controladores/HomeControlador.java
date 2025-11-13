48fc8493a319c64e73c5cc30ab56872cda670f2a
Desesspere <juanjonir@gmail.com>
2025-11-12 19:54:58 -0500

    Corrección JSON HomeScreen

--- src/main/java/com/bibliotecaPersonal/biblioteca_app/Controladores/HomeControlador.java
+++ src/main/java/com/bibliotecaPersonal/biblioteca_app/Controladores/HomeControlador.java
@@ -1,13 +1,19 @@
 package com.bibliotecaPersonal.biblioteca_app.Controladores;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HomeControlador {
 
     @GetMapping("/")
-    public String home() {
-        return "Welcome to Biblioteca Personal API";
+    public ResponseEntity<Map<String, String>> home() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Welcome to Biblioteca Personal API");
+        return ResponseEntity.ok(response);
     }
 }
