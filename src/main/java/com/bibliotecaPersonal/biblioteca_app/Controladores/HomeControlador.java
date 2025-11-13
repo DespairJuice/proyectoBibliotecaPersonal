ea66b34475bc7b260d09f7009bf135a5ae0c7921
Desesspere <juanjonir@gmail.com>
2025-11-12 20:56:47 -0500

    Pruebas conexión backend

--- src/main/java/com/bibliotecaPersonal/biblioteca_app/Controladores/HomeControlador.java
+++ src/main/java/com/bibliotecaPersonal/biblioteca_app/Controladores/HomeControlador.java
@@ -1,21 +1,13 @@
 package com.bibliotecaPersonal.biblioteca_app.Controladores;
 
-import com.bibliotecaPersonal.biblioteca_app.Persistencia.Entidades.Libro;
-import com.bibliotecaPersonal.biblioteca_app.Servicios.LibroServicio;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class HomeControlador {
 
-    @Autowired
-    private LibroServicio libroServicio;
-
-    @GetMapping("/")
-    public List<Libro> home() {
-        return libroServicio.buscarPorTitulo(""); // Devuelve todos si vacío
+    @GetMapping("/api")
+    public String home() {
+        return "Welcome to Biblioteca Personal API";
     }
 }
--- src/main/java/com/bibliotecaPersonal/biblioteca_app/Controladores/LibroControlador.java
+++ src/main/java/com/bibliotecaPersonal/biblioteca_app/Controladores/LibroControlador.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/libros")
+@RequestMapping("/")
 public class LibroControlador {
 
     @Autowired
